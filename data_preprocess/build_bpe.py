1af01ac1597f81005cf86f2e69b2d681ee0b0300
michael <michael_wzhu91@163.com>
2020-02-02 22:08:28 +0800

    add data preproc example

--- data_preprocess/build_bpe.py
+++ data_preprocess/build_bpe.py
@@ -8,12 +8,19 @@ from tokenizers import (ByteLevelBPETokenizer,
 
 if __name__ == "__main__":
     tokenizer_name = "zh_example"
-    tokenizer = ByteLevelBPETokenizer()
+    # tokenizer = ByteLevelBPETokenizer()
+    tokenizer = BertWordPieceTokenizer(
+        handle_chinese_chars=False,
+    )
+    # tokenizer = SentencePieceBPETokenizer()
+
     tokenizer.train(
         [
             "datasets/examples/corpus_zh_example_subchar_0.txt"
         ],
-        vocab_size=500
+        vocab_size=50,
+        min_frequency=1,
+
     )
     tokenizer.save("resources/tokenizer", tokenizer_name)
 
@@ -21,14 +28,21 @@ if __name__ == "__main__":
     # tryout the trained model
     ####################
     # Initialize a tokenizer
-    vocab = "./resources/tokenizer/zh_example-vocab.json"
+    vocab = "./resources/tokenizer/zh_example-vocab.txt"
+    # vocab = "./resources/tokenizer/zh_example-vocab.json"
     merges = "./resources/tokenizer/zh_example-merges.txt"
-    tokenizer = ByteLevelBPETokenizer(vocab, merges)
+    # tokenizer = ByteLevelBPETokenizer(vocab, merges)
     # tokenizer = BPETokenizer(vocab, merges)
+    tokenizer = BertWordPieceTokenizer(vocab, handle_chinese_chars=False)
+    # tokenizer = SentencePieceBPETokenizer(
+    #     vocab,
+    #     merges
+    # )
 
     # And then encode:
     # encoded = tokenizer.encode("南京老城“路在何方”？")
-    encoded = tokenizer.encode("立木斤 开刂土 冖二儿寸 丬犬 疒丙 母 ( 2019 - nCoV )")
+    # encoded = tokenizer.encode("立木斤 开刂土 冖二儿寸 丬犬 疒丙 母 ( 2019 - nCoV )")
+    encoded = tokenizer.encode("丰月 勹丶勹山 乛头 戶方 月邑 占火 金戋 者邑 句多 乛头 一冂丨人人 木东 另刀 野土 乛亅 ")
     print(encoded.ids)
     print(encoded.tokens)
 
