dc3673da62d9d19cb93baf896b97f56655c54606
michael <michael_wzhu91@163.com>
2020-02-16 16:10:09 +0800

    add data preproc example

--- data_preprocess/char2comp_mp.sh
+++ data_preprocess/char2comp_mp.sh
@@ -3,6 +3,6 @@
 ### multi-processing version
 NUM_PROC=11
 for i in `seq 1 $((NUM_PROC))`; do
-  python data_preprocess/char2comp_mp.py datasets/examples/corpus_zh_example_0_${i}.txt datasets/examples/corpus_zh_example_${i}_subchar_lower.txt resources/char2comp.json 1 \
+  python data_preprocess/char2comp_mp.py corpus/zhwiki_articles_${i}.txt corpus/subchar_lower/zhwiki_articles_${i}_subchar_lower.txt resources/char2comp.json 1 \
     $@ &
 done
