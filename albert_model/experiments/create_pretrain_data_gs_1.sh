68776fd065b539f2519de1400edab25704f75618
michael <michael_wzhu91@163.com>
2020-02-19 00:16:47 +0800

    add data preproc example

--- albert_model/experiments/create_pretrain_data_gs_1.sh
+++ albert_model/experiments/create_pretrain_data_gs_1.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 ### multi-processing version
+
+export STORAGE_BUCKET=gs://sbt0
+
 NUM_PROC=12
 for i in `seq 6 $((NUM_PROC))`; do
-  python3 albert_model/create_pretraining_data.py --input_file=./corpus/subchar_lower/zhwiki-latest-pages-articles_${i}_subchar_lower.txt --output_file=./albert_model/experiments/subchar_lower_vocab_5000/zhwiki_train_examples_${i}_%s.tfrecord --vocab_file=./resources/tokenizer/5000-clean.vocab --spm_model_file=./resources/tokenizer/5000-clean.model --do_lower_case=True --do_whole_word_mask=True --max_seq_length=512 --max_predictions_per_seq=52 --masked_lm_prob=0.15 --dupe_factor=5
+  python3 albert_model/create_pretraining_data.py --input_file=./corpus/subchar_lower/zhwiki-latest-pages-articles_${i}_subchar_lower.txt --output_file=${STORAGE_BUCKET}/subchar_lower_vocab_5000/zhwiki_train_examples_${i}_%s.tfrecord --vocab_file=./resources/tokenizer/5000-clean.vocab --spm_model_file=./resources/tokenizer/5000-clean.model --do_lower_case=True --do_whole_word_mask=True --max_seq_length=512 --max_predictions_per_seq=52 --masked_lm_prob=0.15 --dupe_factor=5
 done
