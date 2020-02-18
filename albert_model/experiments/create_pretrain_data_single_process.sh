1aaf4b7a2067368da0213e57936f5d68b960e661
michael <michael_wzhu91@163.com>
2020-02-19 00:09:50 +0800

    add data preproc example

--- albert_model/experiments/create_pretrain_data_single_process.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-### multi-processing version
-NUM_PROC=6
-for i in `seq 5 $((NUM_PROC))`; do
-  python3 albert_model/create_pretraining_data.py --input_file=./corpus/subchar_lower/zhwiki-latest-pages-articles_${i}_subchar_lower.txt --output_file=./albert_model/experiments/subchar_lower_vocab_5000/zhwiki_train_examples_${i}_%s.tfrecord --vocab_file=./resources/tokenizer/5000-clean.vocab --spm_model_file=./resources/tokenizer/5000-clean.model --do_lower_case=True --do_whole_word_mask=True --max_seq_length=512 --max_predictions_per_seq=52 --masked_lm_prob=0.15 --dupe_factor=5
-done
