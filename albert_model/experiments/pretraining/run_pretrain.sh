6b5fa89c74037c8712f9c4b3d3ef2a64116f189f
michael <michael_wzhu91@163.com>
2020-02-20 21:09:24 +0800

    add data preproc example

--- albert_model/experiments/pretraining/run_pretrain.sh
+++ albert_model/experiments/pretraining/run_pretrain.sh
@@ -3,6 +3,6 @@
 ### run pretrain
 
 export STORAGE_BUCKET=gs://sbt0
-export TPU_NAME=michaelwzhu91
+export TPU_NAME=subchar-trans-512
 
-python3 albert_model/run_pretraining.py --input_file=${STORAGE_BUCKET}/subchar_lower_vocab_5000/zhwiki_train_examples_*_*.tfrecord --output_dir=${STORAGE_BUCKET}/uncased_vocab_5000_time_0216/ --albert_config_file=./albert_model/experiments/albert_base_v2_config_vocab_5000.json --do_train --do_eval --dev_input_file=${STORAGE_BUCKET}/subchar_lower_vocab_5000/zhwiki_dev_examples_*_*.tfrecord --use_tpu --num_tpu_cores=8 --tpu_name=${TPU_NAME} --train_batch_size=512 --eval_batch_size=64 --max_seq_length=512 --max_predictions_per_seq=52 --optimizer="lamb" --learning_rate=2e-4 --num_train_steps=62500 --num_warmup_steps=3125 --save_checkpoints_steps=5000
+python3 albert_model/run_pretraining.py --input_file=${STORAGE_BUCKET}/subchar_lower_vocab_5000/zhwiki_train_examples_*_*.tfrecord --output_dir=${STORAGE_BUCKET}/uncased_vocab_5000_time_0216/ --albert_config_file=./albert_model/experiments/albert_base_v2_config_vocab_5000.json --do_train --do_eval --dev_input_file=${STORAGE_BUCKET}/subchar_lower_vocab_5000/zhwiki_dev_examples_*_*.tfrecord --use_tpu --num_tpu_cores=8 --tpu_name=${TPU_NAME} --train_batch_size=1024 --eval_batch_size=64 --max_seq_length=512 --max_predictions_per_seq=52 --optimizer="lamb" --learning_rate=4e-4 --num_train_steps=125000 --num_warmup_steps=3125 --save_checkpoints_steps=5000
