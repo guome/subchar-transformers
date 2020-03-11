7ceb51ac8cea0d44ea2bf3d1c74388143f16503d
michael <michael_wzhu91@163.com>
2020-03-11 12:54:41 +0800

    fix pretrain data generation

--- data_preprocess/build_spm.py
+++ data_preprocess/build_spm.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     tf.gfile.Copy(input_dir_gs, input_dir_local, overwrite=True)
 
 
-    for vocab_size in [5000, 10000]:
+    for vocab_size in [15000]:
 
         spm.SentencePieceTrainer.train('--input=zhwiki-latest-pages-articles_char_lower.txt --model_prefix=./resources/tokenizer/char-%d-clean --vocab_size=%d --pad_id=0 --unk_id=1 --eos_id=-1 --bos_id=-1 --control_symbols=[CLS],[SEP],[MASK] --user_defined_symbols=(,),”,-,.,–,£,€ --shuffle_input_sentence=true --input_sentence_size=30000000 --character_coverage=0.99995 --model_type=bpe' % (vocab_size, vocab_size))
 
