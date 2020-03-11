d1c1e2bd1f9a578b79a562792139349d2f817f6b
michael <michael_wzhu91@163.com>
2020-03-11 14:04:05 +0800

    fix pretrain data generation

--- albert_model/experiments/pretraining_normal_transformers/create_pretraining_data_length_128.sh
+++ albert_model/experiments/pretraining_normal_transformers/create_pretraining_data_length_128.sh
@@ -7,5 +7,6 @@ export STORAGE_BUCKET=gs://sbt0
 NUM_PROC=110
 
 for i in `seq 1 $((NUM_PROC))`; do
-  python3 albert_model/create_pretraining_data.py --input_file=$STORAGE_BUCKET/data/corpus/char_lower/zhwiki-latest-pages-articles_${i}_char_lower.txt --output_file=${STORAGE_BUCKET}/data/char_lower_vocab_15000_length_128/zhwiki_train_examples_${i}_%s.tfrecord --vocab_file=./resources/tokenizer/char-15000-clean.vocab --spm_model_file=./resources/tokenizer/char-15000-clean.model --do_lower_case=True --do_whole_word_mask=True --max_seq_length=128 --max_predictions_per_seq=13 --masked_lm_prob=0.1 --dupe_factor=10
+  python3 albert_model/create_pretraining_data.py --input_file=$STORAGE_BUCKET/data/corpus/char_lower/zhwiki-latest-pages-articles_${i}_char_lower.txt --output_file=${STORAGE_BUCKET}/data/char_lower_vocab_15000_length_128/zhwiki_train_examples_${i}_%s.tfrecord --vocab_file=./resources/tokenizer/char-15000-clean.vocab --spm_model_file=./resources/tokenizer/char-15000-clean.model --do_lower_case=True --do_whole_word_mask=True --max_seq_length=128 --max_predictions_per_seq=13 --masked_lm_prob=0.1 --dupe_factor=10 \
+  $@ &
 done
