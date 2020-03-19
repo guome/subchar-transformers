23ce0829cf9a9cd5e616ea230a3683b3a2a82870
michael <michael_wzhu91@163.com>
2020-03-19 23:46:30 +0800

    update fine-tune results on iflytek

--- data_preprocess/split_file_utils.py
+++ data_preprocess/split_file_utils.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 
     from_file = os.path.join(
         STORAGE_BUCKET,
-        "data/corpus/zhwiki-latest-pages-articles.txt"
+        "data/corpus/char_lower/zhwiki-latest-pages-articles.txt"
     )
     tf.gfile.Copy(from_file, "./zhwiki-latest-pages-articles.txt")
 
