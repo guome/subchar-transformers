7f9bc52703a219b582340540cc00065765f57990
michael <michael_wzhu91@163.com>
2020-03-20 14:25:42 +0800

    fix pretrain data generation

--- data_preprocess/char2char_spaced_mp.sh
+++ data_preprocess/char2char_spaced_mp.sh
@@ -3,8 +3,8 @@
 export STORAGE_BUCKET=gs://sbt0
 
 ### multi-processing version
-NUM_PROC=218
-for i in `seq 141 $((NUM_PROC))`; do
+NUM_PROC=70
+for i in `seq 1 $((NUM_PROC))`; do
   python3 data_preprocess/char2char_spaced_mp.py ${STORAGE_BUCKET}/data/corpus/splited/zhwiki-latest-pages-articles_${i}.txt ${STORAGE_BUCKET}/data/corpus/char_spaced_lower/zhwiki-latest-pages-articles_${i}_char_spaced_lower.txt 1 \
     $@ &
 done
