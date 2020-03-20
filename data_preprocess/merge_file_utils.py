235d42a3a1b4af8c0a57b2c5a6bf959e4d5a00b2
michael <michael_wzhu91@163.com>
2020-03-20 16:48:42 +0800

    fix pretrain data generation

--- data_preprocess/merge_file_utils.py
+++ data_preprocess/merge_file_utils.py
@@ -37,13 +37,13 @@ if __name__ == "__main__":
     for i in range(num_files):
         file_ = os.path.join(
             STORAGE_BUCKET,
-            "data/corpus/subchar_spaced_lower/zhwiki-latest-pages-articles_%d_subchar_spaced_lower.txt" % (i + 1)
+            "data/corpus/subchar_no_space_lower/zhwiki-latest-pages-articles_%d_subchar_no_space_lower.txt" % (i + 1)
         )
         list_files_.append(file_)
 
     to_dir_ = os.path.join(
         STORAGE_BUCKET,
-        "data/corpus/subchar_spaced_lower/zhwiki-latest-pages-articles_subchar_spaced_lower.txt")
+        "data/corpus/subchar_no_space_lower/zhwiki-latest-pages-articles_subchar_no_space_lower.txt")
     txt_files2file(list_files_, to_dir_)
 
     # list_files_ = []
