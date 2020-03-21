0e9a05d32d78f4da7498ed4b9ea3336ac194f317
michael <michael_wzhu91@163.com>
2020-03-21 15:44:36 +0800

    update fine-tune results on iflytek

--- data_preprocess/build_spm.py
+++ data_preprocess/build_spm.py
@@ -33,13 +33,13 @@ if __name__ == "__main__":
 
     STORAGE_BUCKET = "gs://sbt0"
 
-    for prefix in prefixes:
-        input_dir_gs = os.path.join(
-            STORAGE_BUCKET,
-            "data/corpus/%s/zhwiki-latest-pages-articles_%s.txt" % (prefix, prefix)
-        )
-        input_dir_local = "./zhwiki-latest-pages-articles_%s.txt" % prefix
-        tf.gfile.Copy(input_dir_gs, input_dir_local, overwrite=True)
+    # for prefix in prefixes:
+    #     input_dir_gs = os.path.join(
+    #         STORAGE_BUCKET,
+    #         "data/corpus/%s/zhwiki-latest-pages-articles_%s.txt" % (prefix, prefix)
+    #     )
+    #     input_dir_local = "./zhwiki-latest-pages-articles_%s.txt" % prefix
+    #     tf.gfile.Copy(input_dir_gs, input_dir_local, overwrite=True)
 
 
     for vocab_size in vocab_sizes:
