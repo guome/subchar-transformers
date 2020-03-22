60ded188260e59cd4371996984894bb7a5e40e80
michael <michael_wzhu91@163.com>
2020-03-22 16:20:12 +0800

    fix pretrain data generation

--- data_preprocess/build_spm.py
+++ data_preprocess/build_spm.py
@@ -22,12 +22,12 @@ if __name__ == "__main__":
     #
     #     spm.SentencePieceTrainer.train('--input=zhwiki-latest-pages-articles_char_lower.txt --model_prefix=./resources/tokenizer/char-%d-clean --vocab_size=%d --pad_id=0 --unk_id=1 --eos_id=-1 --bos_id=-1 --control_symbols=[CLS],[SEP],[MASK] --user_defined_symbols=(,),”,-,.,–,£,€ --shuffle_input_sentence=true --input_sentence_size=30000000 --character_coverage=0.99995 --model_type=bpe --num_threads=32' % (vocab_size, vocab_size))
 
-    vocab_sizes = [5000, 10000, 15000, 20000, 25000, 30000]
-    # vocab_sizes = [2500, 5000, 10000]
+    # vocab_sizes = [5000, 10000, 15000, 20000, 25000, 30000]
+    vocab_sizes = [5000]
     prefixes = [
-        "char_spaced_lower",
-        "char_no_space_lower",
-        "subchar_spaced_lower",
+        # "char_spaced_lower",
+        # "char_no_space_lower",
+        # "subchar_spaced_lower",
         "subchar_no_space_lower",
     ]
 
