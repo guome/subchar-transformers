1667096bb081652074fc4f079ffa80482f8fff62
michael <michael_wzhu91@163.com>
2020-03-25 19:00:40 +0800

    fix pretrain data generation

---
+++ data_preprocess/char2comp_segmented_mp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export STORAGE_BUCKET=gs://sbt0
+
+### multi-processing version
+NUM_PROC=218
+for i in `seq 1 $((NUM_PROC))`; do
+  python3 data_preprocess/char2comp_segmented_mp.py ${STORAGE_BUCKET}/data/corpus/splited/zhwiki-latest-pages-articles_${i}.txt ${STORAGE_BUCKET}/data/corpus/subchar_segmented_lower/zhwiki-latest-pages-articles_${i}_subchar_segmented_lower.txt resources/ids_dict_char2comps_joined.json 1 \
+    $@ &
+done
