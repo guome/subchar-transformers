62709b132d11089a9150f41d731c181973041c5d
michael <michael_wzhu91@163.com>
2020-03-25 17:56:37 +0800

    fix pretrain data generation

--- data_preprocess/build_spm.py
+++ data_preprocess/build_spm.py
@@ -28,18 +28,19 @@ if __name__ == "__main__":
         # "char_spaced_lower",
         # "char_no_space_lower",
         # "subchar_spaced_lower",
-        "subchar_no_space_lower",
+        # "subchar_no_space_lower",
+        "char_segmented_lower",
     ]
 
     STORAGE_BUCKET = "gs://sbt0"
 
-    # for prefix in prefixes:
-    #     input_dir_gs = os.path.join(
-    #         STORAGE_BUCKET,
-    #         "data/corpus/%s/zhwiki-latest-pages-articles_%s.txt" % (prefix, prefix)
-    #     )
-    #     input_dir_local = "./zhwiki-latest-pages-articles_%s.txt" % prefix
-    #     tf.gfile.Copy(input_dir_gs, input_dir_local, overwrite=True)
+    for prefix in prefixes:
+        input_dir_gs = os.path.join(
+            STORAGE_BUCKET,
+            "data/corpus/%s/zhwiki-latest-pages-articles_%s.txt" % (prefix, prefix)
+        )
+        input_dir_local = "./zhwiki-latest-pages-articles_%s.txt" % prefix
+        tf.gfile.Copy(input_dir_gs, input_dir_local, overwrite=True)
 
 
     for vocab_size in vocab_sizes:
