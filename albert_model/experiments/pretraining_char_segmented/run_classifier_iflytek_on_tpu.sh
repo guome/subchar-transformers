399bbd9447103f3900e03c5a680e140db3f81c91
michael <michael_wzhu91@163.com>
2020-03-27 14:58:03 +0800

    fix pretrain data generation

--- albert_model/experiments/pretraining_char_segmented/run_classifier_iflytek_on_tpu.sh
+++ albert_model/experiments/pretraining_char_segmented/run_classifier_iflytek_on_tpu.sh
@@ -55,7 +55,7 @@ for run_idx in `seq 1 $((RUN_TIMES))`; do
       --learning_rate=2e-5 \
       --warmup_step=400 \
       --save_checkpoints_steps=400 \
-      --train_step=400 \
+      --train_step=800 \
       --use_tpu=True \
       --tpu_name=${TPU_NAME} \
       --num_tpu_cores=1 \
