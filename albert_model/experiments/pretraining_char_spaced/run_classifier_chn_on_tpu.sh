a972f291d9fd9705a2bbf3083b8836395b8c4aba
michael <michael_wzhu91@163.com>
2020-03-27 22:04:51 +0800

    work on pretraining (len=256)

--- albert_model/experiments/pretraining_char_spaced/run_classifier_chn_on_tpu.sh
+++ albert_model/experiments/pretraining_char_spaced/run_classifier_chn_on_tpu.sh
@@ -8,7 +8,7 @@ CURRENT_TIME=$(date "+%Y%m%d-%H%M%S")
 
 export STORAGE_BUCKET=gs://sbt0
 
-export TPU_NAME=picto-trans-1
+export TPU_NAME=grpc://10.86.36.74:8470
 
 TASK_NAME="chn"
 PREFIX=char_spaced_lower
