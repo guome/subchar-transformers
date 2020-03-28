9bff7a416153858704752ac7f08753879a7e7e96
michael <michael_wzhu91@163.com>
2020-03-28 16:03:27 +0800

    fix pretrain data generation

--- albert_model/experiments/pretraining_char_segmented/run_classifier_lcqmc_on_tpu.sh
+++ albert_model/experiments/pretraining_char_segmented/run_classifier_lcqmc_on_tpu.sh
@@ -56,7 +56,7 @@ for run_idx in `seq 1 $((RUN_TIMES))`; do
       --learning_rate=2e-5 \
       --warmup_step=1700 \
       --save_checkpoints_steps=1000 \
-      --train_step=20000 \
+      --train_step=30000 \
       --use_tpu=True \
       --tpu_name=${TPU_NAME} \
       --num_tpu_cores=1 \
