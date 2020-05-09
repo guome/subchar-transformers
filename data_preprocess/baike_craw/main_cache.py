270a91ec309df734b50f6e7f273cbb442efae07c
michael <michael_wzhu91@163.com>
2020-05-09 21:42:04 +0800

    work on pretraining (len=256)

--- data_preprocess/baike_craw/main_cache.py
+++ data_preprocess/baike_craw/main_cache.py
@@ -328,7 +328,7 @@ def main(proc_idx):
 
 
 if __name__ == "__main__":
-    num_processes = 4
+    num_processes = 16
     jobs = []
     for i in range(num_processes):
         # job = multiprocessing.Process(target=main, args=())
